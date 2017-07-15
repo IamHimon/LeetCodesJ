b7b7efd33c0158a545031b92de6b03a6566fbc3c
IamHimon <539916384@qq.com>
2017-07-15 13:00:29 +0800

    Starbucks codes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,21 @@
 package com.company;
 
-import static com.company.Solu.Solution.eight;
+import static com.company.Solu.Solution.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(eight("123"));
+//        System.out.println(eight("2147483648"));
+//        System.out.println(eight("18446744073709551617"));
+//        System.out.println(eight_L("18446744073709551617"));
+//        System.out.println(eight_L("+-1234"));
+//        System.out.println(eight("+-1234"));
+//        char a = 1;
+//        System.out.println(nine(1234));\
+//        System.out.println(-2147483648);
+//        System.out.println(nine(-2147483648));
+        System.out.println(nine_L(12321));
+
     }
 }
--- src/com/company/Solu/Solution.java
+++ src/com/company/Solu/Solution.java
@@ -1,20 +1,25 @@
 package com.company.Solu;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by student on 2017/7/15.
  */
 public class Solution {
     public static int eight(String string) {
+        /*用来判断是不是前一个字符是不是数字，比如“12 342”，让到字符'3'，
+        因为前一个字符是空格，所以is_digitandlabel=false，所以会break掉程序。*/
         boolean is_digitandlabel= true;
         int flag = 1;
         long result = 0;
+        /*num_flag,应对有两个正负号的情况，比如"+-1234",这时候num_flag>1,是错误输入return 0;*/
         int num_flag = 0;
-
         //去掉开头的空格
-        while (string.startsWith(" ")) {//这里判断是不是全角空格
-            string = string.substring(1, string.length()).trim();
-        }
-        System.out.println(string);
+        string.trim();
+//        while (string.startsWith(" ")) {//这里判断是不是空格
+//            string = string.substring(1, string.length()).trim();
+//        }
         for (int i = 0; i < string.length(); i++) {
 //            System.out.println(is_digitandlabel);
             if (string.charAt(i) == ' ') {
@@ -33,17 +38,72 @@ public class Solution {
             else if (Character.isDigit(string.charAt(i))&&is_digitandlabel) {
                 is_digitandlabel = true;
                 result = result * 10 + ((int) string.charAt(i) - 48);
+                if (result>Integer.MAX_VALUE||result<Integer.MIN_VALUE) {
+                    System.out.println("overflow");
+                    return flag==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+                }
             }else {
                 break;
             }
         }
         result = result*flag;
-
-        if (result>Integer.MAX_VALUE||result<Integer.MIN_VALUE)
-            return 0;
         if (num_flag>1)
             return 0;
-
         return (int)result;
     }
+/*
+* java char to int: int x=str1.charAt(0)-'0';
+* 然后判断是不是数字字母： if (x>=0&&x<=9) return true;
+* */
+    public static int eight_L(String str){
+        if (str.length() == 0) return 0;
+        //remove only the leading and trailing white spaces
+        str = str.trim();
+
+        int sign=1,total=0,index=0,len=str.length(),val=0;
+        if (str.charAt(index)=='+'||str.charAt(index)=='-'){
+            sign = str.charAt(index)=='+'?1:-1;
+            index += 1;
+        }
+        while (index<len){
+            val = str.charAt(index)-'0';
+            if (val<0||val>9) break;
+            if (total>Integer.MAX_VALUE/10||(total==Integer.MAX_VALUE/10&&val>Integer.MAX_VALUE%10)){
+                return sign == 1 ? Integer.MAX_VALUE :Integer.MIN_VALUE;
+            }
+            total = total*10 + val;
+            index++;
+        }
+        return sign*total;
+    }
+
+    public static boolean nine(int x){
+        if (x>Integer.MAX_VALUE||x<Integer.MIN_VALUE)
+            return false;
+        Integer remainder=0;
+        ArrayList<Integer> remainders = new ArrayList<>();
+        while (x>0){
+            remainder = x%10;
+            remainders.add(remainder);
+            x = x/10;
+        }
+        for (int i=0;i<remainders.size()/2;i++){
+            if (remainders.get(i) != remainders.get(remainders.size()-i-1))
+                return false;
+        }
+        return true;
+    }
+
+    /*no overflow risk*/
+    public static boolean nine_L(int x){
+        if (x<0) return false;
+        int p=x, q=0;
+        while (p>=10){
+            q *= 10;
+            q = q + p%10;
+            p = p/10;
+        }
+        return q==x/10&&p==x%10;
+    }
+
 }
