222fa3cf3f70f6e4a9ff6b12c970618caf0c8ebb
IamHimon <539916384@qq.com>
2017-08-30 17:27:16 +0800

    WJBank codes

--- src/com/company/BasedClass/Bag.java
+++ src/com/company/BasedClass/Bag.java
@@ -111,6 +111,60 @@ public class Bag {
     private static int Meituan(int N) {
         int[] f = new int[N + 1];
 
+        return 0;
+    }
+
+    /*美团2017,第三题：给定一组非负整数组成的数组h，代表一组柱状图的高度，其中每个柱子的宽度都为1。 在这组柱状图中找到能组成的最大矩形的面积。
+     *入参h为一个整型数组，代表每个柱子的高度，返回面积的值。*/
+    private static int maxArea(int n, int[] num){
+        int max_area = 0;
+        for(int i=0;i<n;i++){
+            int width;
+            //向前
+            int f = i;
+            for (;f>0;f--){
+                if (num[f] < num[i])
+                    break;
+            }
+            //向后
+            int b=i;
+            for(;b<n;b++){
+                if(num[b]< num[i])
+                    break;
+            }
+            width = b - f -1;
+            max_area = max_area>(num[i]*width)?max_area:(num[i]*width);
+        }
+        return max_area;
+    }
+
+
+    /*美团2017，第四题：给出两个字符串（可能包含空格）,找出其中最长的公共连续子串,输出其长度。*/
+    private static int maxLenSameStr(String s1, String s2){
+        if (s1 ==null || s2 == null)
+            return 0;
+        char[] S1 = s1.toCharArray();
+        char[] S2 = s2.toCharArray();
+        //i => s1, j => s2
+        int i =0, j=0;
+        int max_len = 0;
+        int tem_len = 0;
+        while (i<s1.length() || j<s2.length()){
+            if (i<s1.length() && j<s2.length() && (S1[i] == S2[j])){
+                i++;
+                j++;
+                tem_len++;
+            }else if (j<s2.length()){
+                tem_len = 0;
+                j++;
+            }else {
+                tem_len = 0;
+                i++;
+                j=0;
+            }
+            max_len = max_len>tem_len?max_len:tem_len;
+        }
+        return max_len;
     }
 
 
@@ -119,7 +173,10 @@ public class Bag {
         int[] C = new int[]{0, 2, 3, 4};
 //        System.out.println(Package_1(W, C));
 //        System.out.println(Package_2(W, C));
-        System.out.println(Full_Package_1(W, C));
-        System.out.println(Full_Package_2(W, C));
+//        System.out.println(Full_Package_1(W, C));
+//        System.out.println(Full_Package_2(W, C));
+
+//        System.out.println(maxArea(6,new int[]{2,1,5,6,2,3}));
+        System.out.println(maxLenSameStr("word", "word"));
     }
 }
