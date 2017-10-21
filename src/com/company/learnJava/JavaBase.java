903efaebdfc8737753beeb3dced1b91f9e791860
himon <tohimon@hotmail.com>
2017-10-21 18:21:48 +0800

    starbucks codes

--- src/com/company/learnJava/JavaBase.java
+++ src/com/company/learnJava/JavaBase.java
@@ -7,4 +7,21 @@ public class JavaBase {
     /*
     * PriorityQueue
     * */
+
+    private static void testObjectClass(){
+        Object o1 = 1;
+        Object o2 = 1;
+        System.out.println(o1.hashCode());
+        System.out.println(o2.hashCode());
+        System.out.println(o1.equals(o2));
+    }
+
+
+
+    public static void main(String[] args) {
+        testObjectClass();
+
+    }
+
+
 }
